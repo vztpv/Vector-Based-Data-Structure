ad25e76f3c4a4d3f7f10d21864e098085a14a642
DongWook Lee <vztpv@naver.com>
2018-10-19 07:47:06 +0900

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 
+#include <cstdlib>
 #include <ctime>
 
 #include "arr_vector.h"
@@ -11,8 +12,11 @@
 #include <queue>
 #include <map>
 
-#include "wiz/array_map.h"
+#include "wiz/global.h"
+#include "Header.h"
+//#include "wiz/array_map.h"
 #include "wiz/Queues.h"
+//#include "arr_rbtree.h"
 
 void test1();
 void test2();
@@ -25,22 +29,69 @@ int main(void)
 	return 0;
 }
 
+
+class Data
+{
+public:
+	long long first;
+	long long second;
+//	std::string second;
+public:
+	bool operator<(const Data& other) const {
+		return this->first < other.first;
+	}
+	bool operator!=(const Data& other) const {
+		return this->first != other.first;
+	}
+	bool operator==(const Data& other) const {
+		return this->first == other.first;
+	}
+};
 void test2() {
+	srand(0); // time(nullptr));
 
 	int a = clock(); // 
-	for (int i = 0; i < 10000; ++i) {
-		wiz::ArrayMap<long long, long long> x;
-		//std::map<long long, long long> x;
-		const long long MAX = 500;
+	
+
+//	wiz::RB_Tree<Data, wiz::ASC<Data>>* x = new wiz::RB_Tree<Data, wiz::ASC<Data>>();
+
+	wiz::ArrayMap<long long, long long>* x = new wiz::ArrayMap<long long, long long>();
+
+//	std::map<long long, long long>* x = new std::map<long long, long long>();
+
+	const long long MAX = 1000000; // 4 * 1024;
+
+	//(*x)[MAX];
+
+	for (int i = 0; i < 10; ++i) { // 100
+		x->clear(); 
 		//wiz::arr_map<long long, long long> x(5);
+	//	x->reserve(MAX);
 
 		for (long long i = 0; i < MAX; ++i) {
-			x.insert(std::pair(MAX - i, i + 1));
+			std::pair<long long, long long> temp(wiz::Rand2<long long>()%MAX, i + 1); // "abcdabcdabcdabcdabcdabcdabcdabcd"); //MAX - i, i + 1);
+
+			//std::cout << temp.first << " " << temp.second << "\n";
+			x->insert(temp);
 		}
-		for (long long i = 0; i < MAX; ++i) {
-			x[MAX - i];
+		x->update();
+
+		/*for (long long i = 0; i < MAX; ++i) {
+			x->remove(std::pair<long long, long long>(i, 0));
+			//x->erase(i);
+		}*/
+	}
+	
+	/*
+	for (int i = 0; i < x->size(); ++i) {
+		if (x->find(i) != x->end()) {
+			std::cout << (*x)[i] << "\n";
 		}
 	}
+	*/
+
+	delete x;
+
 	int b = clock();
 	std::cout << b - a << "ms" << std::endl;
 }
